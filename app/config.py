b33b08ec241026fafa8db30635b8f195a9e9b545
someone <>
2025-03-12 16:49:42 +0800

    Fix log directory

--- app/config.py
+++ app/config.py
@@ -10,6 +10,6 @@ class Config:
     PRIVATE_KEY = os.getenv("PRIVATE_KEY")
     ADDRESS = os.getenv("ADDRESS")
     PUBKEY = os.getenv("PUBKEY")
-    LOG_FILE = "logs/boost.log"
+    LOG_FILE = "boost.log"
     PORT = int(os.getenv("PORT",5010))
     INTERVAL = int(os.getenv("INTERVAL",30))
